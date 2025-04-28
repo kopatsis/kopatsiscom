4265419b9c2b6e16cb5e0ac5fcbc7d5d93feb1a9
James Kopatsis <jkopatsis@gmail.com>
2025-04-27 21:19:49 -0700

    changes to email form

--- assets/js/emailform.js
+++ assets/js/emailform.js
@@ -1,46 +1,77 @@
 document.addEventListener("DOMContentLoaded", () => {
-    document.getElementById("submitButton").addEventListener("click", function (e) {
+  document
+    .getElementById("submitButton")
+    .addEventListener("click", function (e) {
       e.preventDefault();
-  
+
       const form = document.getElementById("contactForm");
       const formData = new FormData(form);
       const formObj = {};
       formData.forEach((value, key) => {
         formObj[key] = value.trim();
       });
-  
+
       if (!formObj.name) {
         document.getElementById("errorMessage").style.display = "block";
-        document.getElementById("errorMessage").textContent = "Please fill out your name.";
+        document.getElementById("errorMessage").textContent =
+          "Please fill out your name.";
         return;
       }
       if (!formObj.email) {
         document.getElementById("errorMessage").style.display = "block";
-        document.getElementById("errorMessage").textContent = "Please fill out your email.";
+        document.getElementById("errorMessage").textContent =
+          "Please fill out your email.";
         return;
       }
       if (!formObj.subject) {
         document.getElementById("errorMessage").style.display = "block";
-        document.getElementById("errorMessage").textContent = "Please fill out the subject.";
+        document.getElementById("errorMessage").textContent =
+          "Please fill out the subject.";
         return;
       }
       if (!formObj.comment) {
         document.getElementById("errorMessage").style.display = "block";
-        document.getElementById("errorMessage").textContent = "Please fill out your message.";
+        document.getElementById("errorMessage").textContent =
+          "Please fill out your message.";
         return;
       }
-  
+
       document.querySelector(".contact-form").style.display = "none";
       document.getElementById("loadingMessage").style.display = "block";
       document.getElementById("errorMessage").style.display = "none";
       document.getElementById("successMessage").style.display = "none";
-  
-      console.log(formObj);
-  
-      setTimeout(() => {
-        document.getElementById("loadingMessage").style.display = "none";
-        document.querySelector(".contact-form").style.display = "none";
-        document.getElementById("successMessage").style.display = "block";
-      }, 1500);
+
+      const postURL = "https://emaildrop.kopatsis.com/contact"
+
+      fetch(postURL, {
+        method: "POST",
+        body: formData,
+      })
+        .then((response) => {
+          if (response.status === 204) {
+            document.getElementById("loadingMessage").style.display = "none";
+            document.querySelector(".contact-form").style.display = "none";
+            document.getElementById("successMessage").style.display = "block";
+          } else {
+            return response.json().then((data) => {
+              if (data.error) {
+                document.getElementById("errorMessage").style.display = "block";
+                document.getElementById("errorMessage").textContent =
+                  data.error;
+              } else {
+                document.getElementById("errorMessage").style.display = "block";
+                document.getElementById("errorMessage").textContent =
+                  "Server error, please try again later.";
+              }
+            });
+          }
+        })
+        .catch(() => {
+          document.getElementById("loadingMessage").style.display = "none";
+          document.querySelector(".contact-form").style.display = "none";
+          document.getElementById("errorMessage").style.display = "block";
+          document.getElementById("errorMessage").textContent =
+            "Sorry, I'm unable to process your form submission request at the moment. Please email me directly, thanks :).";
+        });
     });
-  });
+});
